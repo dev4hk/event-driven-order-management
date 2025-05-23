2640656de244a398491e3585d7433909019a9407
Hoon <hoon0661@gmail.com>
2025-05-23 15:44:20 -0400

    feature/2 - added axon config

--- customer-service/src/main/java/org/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/example/customerservice/CustomerServiceApplication.java
@@ -1,9 +1,12 @@
 package org.example.customerservice;
 
+import org.example.common.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AxonConfig.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
